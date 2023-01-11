1f163788ec6a074c943ff3a83af2ca7eb2bab827
pertusatti22 <pertusatti22@gmail.com>
2023-01-11 14:29:00 -0300

    add results

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -43,13 +43,29 @@ export class QuizzComponent implements OnInit {
     this.nextStep()
   }
 
-  nextStep() {
+  async nextStep() {
     this.questionIndex+=1
     if(this.questionMaxIndex>this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex]
     } else {
+      const finalAnswer: string = await this.checkResult(this.answers)
       this.finished = true
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
     }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ) {
+          return previous
+      } else {
+        return current
+      }
+    })
+    return result
   }
 
 }
